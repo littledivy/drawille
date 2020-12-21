fb8bc5541d70ec5874e9dd51222e2f7c61d4b303
divy-work <dj.srivastava23@gmail.com>
2020-12-21 13:53:09 +0530

    feat(examples): add mouse movement canvas examples via Autopilot

---
+++ examples/mouse_rect_scale.ts
@@ -0,0 +1,35 @@
+// NOTE: Autopilot is an --unstable plugin. We're using it for global mouse movement detection.
+//       Ideally one would use an ncurses-like library.
+import {
+  runMousePosition,
+  runScreenSize,
+} from "https://deno.land/x/autopilot/autopilot_plugin/index.ts";
+import Canvas from "../canvas.ts";
+
+let c = new Canvas();
+let ctx = c.getContext("2d");
+
+interface Screen {
+  width: number;
+  height: number;
+}
+
+let dimensions: Screen = JSON.parse(await runScreenSize());
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+let m: Point;
+
+setInterval(async () => {
+  m = JSON.parse(await runMousePosition());
+  ctx.clear();
+  let x = (m.x / dimensions.width) * c.width;
+  let y = (m.y / dimensions.height) * c.height;
+  ctx.fillRect(10, 10, x, y);
+  ctx.stroke();
+
+  console.log(ctx.toString());
+}, 100);
